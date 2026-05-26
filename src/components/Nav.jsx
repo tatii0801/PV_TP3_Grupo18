4e6982c18c7c8a230e1847e4df1ecd375816663e
MoisesFloresNavajas <moisesfloresnavajas@gmail.com>
2026-05-26 19:33:33 -0300

    incorporación del archivo Nav.jsx

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+const Nav = () => {
+  return (
+    <nav className="contenedor margen">
+      <a href="#">Inicio</a>
+      <a href="#">Proyectos</a>
+      <a href="#">Perfil</a>
+      <a href="#">Detalles</a>
+    </nav>
+  )
+}
+
+export default Nav;
