dcf3b89f94ee7ae30504221b0f67e88d7feebc59
tatii0801 <tatiananieva0801@gmail.com>
2026-06-11 19:33:04 -0300

    Se implemento el React Bootstrap en formularios, tarjetas y alertas

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
 const Footer = () => {
   return (
     <footer>
+    <h6>
       <p>
         Diseñado por el Grupo 18 de Programación Visual - Tiene todos los
         derechos reservados - 2026
       </p>
       <p>Facultad de Ingeniería - Analista Programador Universitario</p>
+    </h6>
     </footer>
   );
 };
