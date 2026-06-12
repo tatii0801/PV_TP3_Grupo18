96c518ccfca3be37f8e9855609cbe519feec3d0c
MoisesFloresNavajas <moisesfloresnavajas@gmail.com>
2026-06-12 17:52:53 -0300

    modificacion de Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,25 @@
 import Nav from "react-bootstrap/Nav";
+import { NavLink } from "react-router-dom";
 
 const Menu = () => {
   return (
     <Nav className="ms-auto" variant="tabs">
       <Nav.Item>
-        <Nav.Link href="#">Inicio</Nav.Link>
+        <Nav.Link as={NavLink} to="/">
+          Inicio
+        </Nav.Link>
       </Nav.Item>
 
       <Nav.Item>
-        <Nav.Link href="#">Proyectos</Nav.Link>
+        <Nav.Link as={NavLink} to="/proyectos">
+          Proyectos
+        </Nav.Link>
       </Nav.Item>
 
       <Nav.Item>
-        <Nav.Link href="#">Perfil</Nav.Link>
-      </Nav.Item>
-
-      <Nav.Item>
-        <Nav.Link href="#">Detalles</Nav.Link>
-      </Nav.Item>
-
-      <Nav.Item>
-        <Nav.Link href="#"> Agregar y Listar Proyectos</Nav.Link>
+        <Nav.Link as={NavLink} to="/perfil">
+          Perfil
+        </Nav.Link>
       </Nav.Item>
     </Nav>
   );
